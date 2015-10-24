2a96dd198a8cfad93d9aee1c0419ebcb1ac78a02
Raül Pérez <repejota@gmail.com>
2015-10-24 02:28:50 +0200

    Added travis badges

--- timeago.go
+++ timeago.go
@@ -1,18 +1,21 @@
 package timeago
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 // TimeAgo ...
 func TimeAgo(from time.Time) (s string) {
-	/*
-		duration := time.Now().Sub(from)
-		years := duration.Hours() / 24 / 365
 
-			if years > 1 {
-				fmt.Println("foooobar")
-			}
-			syears := strconv.FormatFloat(years, 'f', -1, 64)
-			return string(syears)
+	duration := time.Now().Sub(from)
+	fmt.Println(duration.Seconds())
+	/*
+		if years > 1 {
+			fmt.Println("foooobar")
+		}
+		syears := strconv.FormatFloat(years, 'f', -1, 64)
+		return string(syears)
 	*/
 	return ""
 }
